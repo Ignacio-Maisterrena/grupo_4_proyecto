36d8ce6014b90a77c8bedc9a3ced3430da30c524
Ignacio-Maisterrena <you@example.com>
2021-06-17 18:51:11 -0300

    estructura básica

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const publicPath = path.resolve(__dirname, './public');
+
+const app = express();
+app.use( express.static(publicPath));
+
+app.listen(3000, () => {
+    console.log('El servoidor está corriendo en el puerto 3000');
+});
+
+app.get('/', (req, res) => {
+    let homePath = path.resolve(__dirname, './views/home.html');
+    res.sendFile(homePath);
+})
+
+app.get('/login', (req, res) => {
+    let loginPath = path.resolve(__dirname, './views/login.html');
+    res.sendFile(loginPath);
+})
+
+app.get('/register', (req, res) => {
+    let registerPath = path.resolve(__dirname, './views/register.html');
+    res.sendFile(registerPath);
+})
+
