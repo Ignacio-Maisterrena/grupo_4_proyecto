53550528fc58e4c6b4d84d1b736fdfa8b0efd142
santiagofontana00 <santiagofontana00@gmail.com>
2021-09-09 19:41:01 -0300

    Descripición 20 caracteres

--- middlewares/productsValidatorMiddleware.js
+++ middlewares/productsValidatorMiddleware.js
@@ -7,6 +7,7 @@ const { body, validationResult } = require('express-validator')
 
 const validations = [
     body('nombre').isString().isLength({ min: 2 }).withMessage('Debe tener al menos 2 caracteres'),
+    body('descripcion').isString().isLength({ min: 20 }).withMessage('Debe tener al menos 20 caracteres'),
     body('picture').custom((value, { req }) => {
         let file = req.file;
         let acceptedExtensions = ['.jpg', '.png', '.gif']
