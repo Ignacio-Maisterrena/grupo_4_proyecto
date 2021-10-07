91993df1e27e77262f0115c8de7b7eaea4f9e790
Valentin Gallardo <valengallardo2@gmail.com>
2021-10-07 15:54:44 -0300

    Agregado data.sql para importar informacion a la base de datos

---
+++ database/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `database_db`.`tabla_categorias` (`id`, `categoria`) VALUES ('1', 'Buzo');
+
+INSERT INTO `database_db`.`tabla_colores` (`id`, `color`) VALUES ('1', 'Naranja');
+
+INSERT INTO `database_db`.`tabla_talles` (`id`, `talle`) VALUES ('1', 'XS');
+
+INSERT INTO `database_db`.`tabla_productos` (`id`, `nombre_producto`, `imagen`, `precio`, `id_talle`, `id_color`, `descripcion`, `id_categoria`) VALUES ('1', 'busito.90', '1630330743244_img_.jpg', '1234', '1', '1', 'busito', '1');
