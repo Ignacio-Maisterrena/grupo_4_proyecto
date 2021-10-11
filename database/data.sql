9cccf40302aae8ff7a640aa41909e0c28af66e39
Valentin Gallardo <valengallardo2@gmail.com>
2021-10-11 19:28:23 -0300

    Modificados los archivos sql

--- database/data.sql
+++ database/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO `database_db`.`tabla_productos` (`id`, `nombre_producto`, `imagen`, `precio`, `id_talle`, `id_color`, `descripcion`, `id_categoria`) VALUES ('1', 'busito.90', '1630330743244_img_.jpg', '1234', '1', '1', 'lorem ipsum', '1');
-INSERT INTO `database_db`.`tabla_productos` (`id`, `nombre_producto`, `imagen`, `precio`, `id_talle`, `id_color`, `descripcion`, `id_categoria`) VALUES ('2', 'Chaleco Jean', '1630330818453_img_.jpg', '1234', '1', '4', 'lorem ipsum', '2');
-INSERT INTO `database_db`.`tabla_productos` (`id`, `nombre_producto`, `imagen`, `precio`, `id_talle`, `id_color`, `descripcion`, `id_categoria`) VALUES ('3', 'Jean Mujer', '1630330857757_img_.jpg', '1234', '1', '2', 'lorem ipsum', '3');
-INSERT INTO `database_db`.`tabla_productos` (`id`, `nombre_producto`, `imagen`, `precio`, `id_talle`, `id_color`, `descripcion`, `id_categoria`) VALUES ('4', 'Mono', '1630330902632_img_.jpg', '1234', '1', '4', 'lorem ipsum', '4');
-INSERT INTO `database_db`.`tabla_productos` (`id`, `nombre_producto`, `imagen`, `precio`, `id_talle`, `id_color`, `descripcion`, `id_categoria`) VALUES ('5', 'Remera London', '1630361040646_img_.jpg', '1234', '2', '3', 'lorem ipsum', '5');
-INSERT INTO `database_db`.`tabla_productos` (`id`, `nombre_producto`, `imagen`, `precio`, `id_talle`, `id_color`, `descripcion`, `id_categoria`) VALUES ('6', 'Short deportivo prueba 3', '1630330995243_img_.jpg', '1234', '2', '4', 'lorem ipsum', '6');
+INSERT INTO `database_db`.`tabla_productos` (`id`, `nombre_producto`, `imagen`, `precio`, `id_talle`, `id_color`, `descripcion`, `id_categoria`) VALUES ('1', 'Buzo Vintage', '1630330743244_img_.jpg', '3200', '1', '1', 'lorem ipsum', '1');
+INSERT INTO `database_db`.`tabla_productos` (`id`, `nombre_producto`, `imagen`, `precio`, `id_talle`, `id_color`, `descripcion`, `id_categoria`) VALUES ('2', 'Chaleco Jean', '1630330818453_img_.jpg', '4500', '1', '4', 'lorem ipsum', '2');
+INSERT INTO `database_db`.`tabla_productos` (`id`, `nombre_producto`, `imagen`, `precio`, `id_talle`, `id_color`, `descripcion`, `id_categoria`) VALUES ('3', 'Jean Mujer', '1630330857757_img_.jpg', '5200', '1', '2', 'lorem ipsum', '3');
+INSERT INTO `database_db`.`tabla_productos` (`id`, `nombre_producto`, `imagen`, `precio`, `id_talle`, `id_color`, `descripcion`, `id_categoria`) VALUES ('4', 'Mono', '1630330902632_img_.jpg', '3600', '1', '4', 'lorem ipsum', '4');
+INSERT INTO `database_db`.`tabla_productos` (`id`, `nombre_producto`, `imagen`, `precio`, `id_talle`, `id_color`, `descripcion`, `id_categoria`) VALUES ('5', 'Remera London', '1630361040646_img_.jpg', '1200', '2', '3', 'lorem ipsum', '5');
+INSERT INTO `database_db`.`tabla_productos` (`id`, `nombre_producto`, `imagen`, `precio`, `id_talle`, `id_color`, `descripcion`, `id_categoria`) VALUES ('6', 'Short deportivo', '1630330995243_img_.jpg', '2500', '2', '4', 'lorem ipsum', '6');
 
 
 INSERT INTO `database_db`.`tabla_categorias` (`id`, `categoria`) VALUES ('1', 'Buzo');
